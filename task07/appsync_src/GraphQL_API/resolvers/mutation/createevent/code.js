f989d2d377d8786ffba85ffe7f163de5207c88c1
Admin <rysenka2004@gmail.com>
2025-04-05 12:35:45 +0300

    task07

--- task07/appsync_src/GraphQL_API/resolvers/mutation/createevent/code.js
+++ task07/appsync_src/GraphQL_API/resolvers/mutation/createevent/code.js
@@ -2,16 +2,33 @@ import { util } from '@aws-appsync/utils';
 import * as ddb from '@aws-appsync/utils/dynamodb';
 
 export function request(ctx) {
+    const id = util.autoId();
+    const createdAt = util.time.nowISO8601();
+    const payLoadRaw = ctx.args.payLoad;
+
     const item = {
-        id: util.autoId(),
+        id,
         userId: ctx.args.userId,
-        createdAt: util.time.nowISO8601(),
-        payLoad: JSON.parse(ctx.args.payLoad)
+        createdAt,
+        payLoad: util.dynamodb.toMap(payLoadRaw)
     };
 
-    return ddb.put({ key: { id: item.id }, item });
+    ctx.stash.id = id;
+    ctx.stash.createdAt = createdAt;
+
+    return ddb.put({ key: { id }, item });
 }
 
 export function response(ctx) {
-    return ctx.result;
+    if (ctx.error) {
+        return util.error(ctx.error.message, ctx.error.type);
+    }
+
+    return {
+        id: ctx.stash.id,
+        userId: ctx.args.userId,
+        createdAt: ctx.stash.createdAt,
+        payLoad: ctx.args.payLoad 
+    };
 }
+
